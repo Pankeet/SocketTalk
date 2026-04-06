f41c12707b36f82e7e7bbfb0d40765df82b29b53
Pankeet <pankeet04@gmail.com>
2026-04-06 15:45:58 +0530

    Update 2.3

--- fe/src/components/ui/LandingPage.tsx
+++ fe/src/components/ui/LandingPage.tsx
@@ -1,12 +1,10 @@
 import { useState , useRef , useLayoutEffect } from 'react';
-import Send from '../icons/getStarted';
 import gsap from 'gsap';
 import { useNavigate } from 'react-router-dom';
 
 function HomePage() {
 
   const [onbutton, setOnButton] = useState(false);
-  const sendSvg = useRef<HTMLDivElement>(null);
   const welcome = useRef<HTMLDivElement>(null);
   const buttonRef = useRef<HTMLButtonElement>(null);
   const nav = useNavigate();
@@ -21,46 +19,39 @@ function HomePage() {
   }, []);
 
   function getStarted(){
-    setOnButton(true);
-    gsap.to(sendSvg.current,{
-      opacity : 0,
-      x:600,
-      duration : 4,
-      delay : 0.3,
-      ease : "power2.out"
-    });
+    setTimeout(() => {
+        setOnButton(true);
+    }, 500);
 
     gsap.to(welcome.current,{
       opacity:0,
-      duration:4,
-      delay : 0.4,
-      scale : 0.5,
-      y : -20
+      duration:1.8,
+      scale : 0.4,
     });
 
     gsap.to(buttonRef.current ,{
       scale : 1.5,
       duration : 3,
+      delay : 0.5,
     });
 
     setTimeout(() => {
         nav('/dashboard');
-    } , 2500);
+    } , 2400);
     
   }
 
   return (
-    <div className="overflow-hidden min-h-screen w-full bg-gray-950 text-white flex flex-col justify-center items-center">
+    <div className="h-screen w-full bg-gradient-to-b from-slate-900 to-gray-950 text-white flex flex-col justify-center items-center">
       <span ref={welcome} className="text-5xl">
         Welcome to SocketTalk
       </span>
       <div className='flex'>
         <div>
-        <button ref={buttonRef} onClick={getStarted} className={onbutton ? `text-2xl` :`text-xl mt-7 bg-white text-black px-3 py-2 rounded-lg hover:shadow-lg hover:shadow-amber-200 transition-all duration-200`}>
-          {onbutton ?  "Starting...." : "Get Started ?" }
+        <button ref={buttonRef} onClick={getStarted} className={onbutton ? `text-3xl` :`text-xl mt-7 bg-white text-black px-3 py-2 rounded-lg hover:shadow-lg hover:shadow-purple-800 transition-shadow duration-300`}>
+          {onbutton ?  "Starting Socket Engine" : "Get Started ?" }
         </button>
         </div>
-        <div ref={sendSvg} className='mt-10 ml-3 cursor-pointer' onClick={getStarted}><Send /></div>
       </div>
     </div>
   );
