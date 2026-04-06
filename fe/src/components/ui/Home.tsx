0d75fb195b1a32e7a17168fcd323114649b3a3a7
Pankeet <pankeet04@gmail.com>
2026-04-06 15:29:58 +0530

    Update 2.2

--- fe/src/components/ui/Home.tsx
+++ fe/src/components/ui/Home.tsx
@@ -26,7 +26,6 @@ export default function Home(){
             duration : 0.6,
             delay : 0.1
         });
-        console.log(roomID.current?.value, joinID.current?.value, userId.current?.value);
         setTimeout(() => {
             nav('/chat',{
                 state : {
@@ -83,7 +82,7 @@ export default function Home(){
         }   
     }, [connected]);
     return (
-        <div className="h-screen w-full grid place-content-center text-white bg-gradient-to-r from-gray-950 to-black">
+        <div className="h-screen w-full grid place-content-center text-white bg-gradient-to-b from-slate-900 to-black">
             {!create && !join && <div ref={createRoom} className="flex gap-16 border border-white p-4">
                 <div>
                     <button onClick={() => {
@@ -113,7 +112,7 @@ export default function Home(){
                     </button>
                 </div>
             </div>}
-            {create && <div ref={creatingRoom} className="font-serif border border-white p-4 flex flex-col justify-center items-center">
+            {create && <div ref={creatingRoom} className="font-serif border border-white py-8 px-10 rounded-xl flex flex-col justify-center items-center">
                     <div> <span className="text-xl p-3" >Enter Your RoomId to create Custom Room</span></div>
                        <div>
                        <input
@@ -131,31 +130,37 @@ export default function Home(){
                             focus:outline-none focus:ring-1 focus:ring-purple-500 focus:border-purple-500 transition-all duration-300 shadow-lg hover:shadow-purple-500/20"
                             />  
                         </div>
+                        <div className="mt-3">
+                            <span className="cursor-help">Already have a room? <u className="cursor-pointer" onClick={()=> {setCreate(false);setjoin(true)}}>Join</u></span>
+                        </div>
                     <div>
-                        <button type='submit' onClick={CreateRoomfn} className="border-white text-amber-400 mt-4 px-4 py-2 border rounded-lg hover:shadow-md hover:shadow-amber-200 transition-all duration-300">Create</button>
+                        <button type='submit' onClick={CreateRoomfn} className="hover:scale-105 border-white text-amber-400 mt-4 px-4 py-2 border rounded-lg hover:shadow-md hover:shadow-amber-200 transition-all duration-300">Create</button>
                     </div>
                 </div>}
                 {join && <div ref={creatingRoom} className="font-serif border border-white px-10 py-7 flex flex-col justify-center items-center">
                     <div> <span className="text-xl" >Enter roomId to join</span></div>
                        <div>
-                       <input
-                            type="text"
-                            placeholder="room id"
-                            ref={joinID}
-                            className="mt-5 w-full px-4 py-2 bg-transparent border border-gray-700 text-white placeholder-gray-500 rounded-xl 
-                            focus:outline-none focus:ring-1 focus:ring-purple-500 focus:border-purple-500 transition-all duration-300 shadow-lg hover:shadow-purple-500/20"
-                            />
-                            <input 
-                            type="text"
-                            placeholder="User Name"
-                            ref={userId}
-                            className="mt-5 w-full px-4 py-2 bg-transparent border border-gray-700 text-white placeholder-gray-500 rounded-xl 
-                            focus:outline-none focus:ring-1 focus:ring-purple-500 focus:border-purple-500 transition-all duration-300 shadow-lg hover:shadow-purple-500/20"
-                            />  
+                        <input
+                                type="text"
+                                placeholder="room id"
+                                ref={joinID}
+                                className="mt-5 w-full px-4 py-2 bg-transparent border border-gray-700 text-white placeholder-gray-500 rounded-xl 
+                                focus:outline-none focus:ring-1 focus:ring-purple-500 focus:border-purple-500 transition-all duration-300 shadow-lg hover:shadow-purple-500/20"
+                                />
+                                <input 
+                                type="text"
+                                placeholder="User Name"
+                                ref={userId}
+                                className="mt-5 w-full px-4 py-2 bg-transparent border border-gray-700 text-white placeholder-gray-500 rounded-xl 
+                                focus:outline-none focus:ring-1 focus:ring-purple-500 focus:border-purple-500 transition-all duration-300 shadow-lg hover:shadow-purple-500/20"
+                                />  
                         </div>
-                    <div>
-                        <button type='submit' onClick={CreateRoomfn} className="border-white text-amber-400 mt-4 px-4 py-2 border rounded-lg hover:shadow-md hover:shadow-amber-200 transition-all duration-300">Join</button>
-                    </div>
+                        <div className="mt-3">
+                            <span className="cursor-help">Does not have a room? <u className="cursor-pointer" onClick={()=> {setCreate(true);setjoin(false)}}>Create</u></span>
+                        </div>
+                        <div>
+                            <button type='submit' onClick={CreateRoomfn} className="border-white text-amber-400 mt-4 px-4 py-2 border rounded-lg hover:shadow-md hover:shadow-amber-200 transition-all duration-300">Join</button>
+                        </div>
                 </div>}
         </div>
     )
